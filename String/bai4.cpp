507030c5ad274249a57ea53dd3f5f374b7aa8edc
newbiecoder187 <127210576+newbiecoder187@users.noreply.github.com>
2023-07-24 20:10:07 +0700

    Update bai4.cpp

--- String/bai4.cpp
+++ String/bai4.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(){
 	string a; getline(cin,a);
 	string name;
+	// Nhập giá trị ngược cho xâu tên
 	for (int i = a.length() - 1; i > -1; i--){
 		if (a[i] == ' '){
 			break;
@@ -13,10 +14,12 @@ int main(){
 		name = name + a[i];
 		j++;
 	}
+	// In phần họ và tên đệm
 	for (int i = 0; i < a.length() - name.length() ; i++){
 		cout << a[i];
 	}
 	cout << endl;
+	// In phần tên
 	for (int i = name.length() - 1; i > -1; i--){
 		cout << name[i];
 	}
